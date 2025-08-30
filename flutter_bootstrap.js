d561afa722e1b191f77eef9db84d34432b8a1d49
NeaHost <mineamonster@gmail.com>
2025-08-31 01:37:32 +0700

    update ui

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "880456521"
+    serviceWorkerVersion: "368917571"
   }
 });
